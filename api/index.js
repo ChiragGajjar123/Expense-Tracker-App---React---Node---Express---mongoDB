7e5cac36e942175b7f7757dc6b9fa65b83c59dd4
Chirag Gajjar <chiraggajjar421@gmail.com>
2026-04-22 17:56:15 +0530

    Fix bug

--- api/index.js
+++ api/index.js
@@ -13,6 +13,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 // MongoDB Connection
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
